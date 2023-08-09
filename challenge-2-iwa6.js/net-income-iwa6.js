4b8b6af11eff242bf60cbaa145ea53fae22ce58a
seanc0der <sean.noko@gmail.com>
2023-08-09 23:18:29 +0200

    Updated the net-income.js file by amending the codebase, thus resolving issues with the code to produce the desired result as per the challenge requirements

--- challenge-2-iwa6.js/net-income-iwa6.js
+++ challenge-2-iwa6.js/net-income-iwa6.js
@@ -1,5 +1,5 @@
 const rent = 400;
-const tax = '8%';
+const tax = "8%";
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
@@ -8,10 +8,26 @@ const minuteOfDay = 00;
 
 // Only change below this line
 
-if hourOfDay && (minuteOfDay !== null) && (hourOfDay == '00') && (minuteOfDay == '00') {
-	const taxAsDecimal = tax / '100'
-  const startingAfterTax = salary * '1' - taxAsDecimal
-	const balace = starting - transport - food - rent
-}
-	
-console.log(balance.toFixed(3))
+net_income_calc = function (decimals = 4) {
+	let balance;
+	const default_decimals = 4;
+
+	if (decimals === "undefined") {
+		decimals = default_decimals;
+	}
+
+	if (hourOfDay === 0 && minuteOfDay === 0) {
+		const tax_as_decimal = Number(tax.replace("%", "")) / 100;
+		const salary_after_tax = salary * (1 - tax_as_decimal);
+		const net_income = salary_after_tax - rent - food - transport;
+		balance = parseFloat(net_income.toFixed(default_decimals));
+	}
+
+	if (typeof balance === "number") {
+		console.log("R" + balance.toFixed(decimals));
+	} else {
+		console.log("Balance:", (balance = null));
+	}
+};
+
+net_income_calc(2);
